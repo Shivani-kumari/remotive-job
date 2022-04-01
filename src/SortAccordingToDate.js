13a0807da787c5e51f1e271a81387c025332bcdd
Shivani-kumari <shivanikumari5@yahoo.com>
2022-04-01 21:38:32 +0530

    Assignment

---
+++ src/SortAccordingToDate.js
@@ -0,0 +1,9 @@
+import {Button} from 'react-bootstrap'
+import React from 'react'
+
+export default function SortAccordingToDate({jobs}) {
+    
+  return (
+    <Button>Sort According To Date</Button>
+  )
+}
